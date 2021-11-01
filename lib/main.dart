3fb2b3b1f5fdc66c801398b67d8338c2be9f15dd
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-11-01 16:46:11 +0100

    initialized to do repository

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'Homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  static final String title = "Todo App";
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: Homepage(),
+    );
+  }
+}
