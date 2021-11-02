b27d9ef8b7e31390b2a7113446c1abe1c5207ce2
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2021-11-02 01:16:53 +0100

    pagina che mostra todo completati recuperandoli da lista in provider e creando per ognuno un todowidget

---
+++ lib/widgets/completedtodowidget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:mytodoapp/provider/todos.dart';
+import 'package:mytodoapp/widgets/todowidget.dart';
+import 'package:provider/provider.dart';
+
+class Completedtodowidget extends StatelessWidget {
+  const Completedtodowidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final provider =
+        Provider.of<Todosprovider>(context); //per avere accesso a provider
+    final alltodos = provider.todoscompleted;
+    //per ottenere lista todo non fatti
+    return alltodos.isEmpty
+        ? Center(
+            child: Text(
+              'Nessun todo completato', //nel caso in cui non ho todo
+              style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontSize: 14,
+              ),
+            ),
+          )
+        : ListView.separated(
+            physics: BouncingScrollPhysics(),
+            padding: EdgeInsets.all(10),
+            itemBuilder: (context, index) {
+              final todo = alltodos[index];
+              return Todowidget(todo: todo);
+            },
+            separatorBuilder: (context, index) {
+              /*return Divider(
+          color: Colors.grey[300],
+          indent: 20,
+          endIndent: 20,
+          thickness: 2,
+        );*/
+              return SizedBox(
+                height: 8,
+              );
+            },
+            itemCount: alltodos.length,
+          );
+  }
+}
